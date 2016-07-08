68eda0855b02dc8be8ca91f4c2087cc9ea29106c
Amit Onkare <amit.onkare@gmail.com>
2016-07-08 19:24:31 +0530

    stopwatch and countdown timer in javascript.

--- script.js
+++ script.js
@@ -1,101 +1,142 @@
-$(document).ready(function() {
-	// $('#demo1').stopwatch().stopwatch('start');
-	// $('#demo2').stopwatch().click(function(){ 
-	// 	$(this).stopwatch('toggle');
-	// });
-	// $('#demo3').stopwatch().click(function(){ 
-	// 	$(this).stopwatch('reset');
-	// }).stopwatch('start');
-	// $('#demo4').stopwatch({startTime: 10000000}).stopwatch('start');
-	// $('#demo5').stopwatch({updateInterval: 2000}).stopwatch('start');
-	// $('#demo6').stopwatch({format: '{Minutes} and {s.}'}).stopwatch('start');
-//////////////////////
+(function() {
+	// Stopwatch
+	var h1 = document.getElementById('stopwatchTimer'),
+		start = document.getElementById('startSW'),
+		stop = document.getElementById('stopSW'),
+		clear = document.getElementById('resetSW'),
+		switch_sw = document.getElementById('stopwatchBtn'),
+		switch_cd = document.getElementById('countdownBtn'),
+		msecs = 0, minutes = 0, secs = 0, t,
+		stopwatchDiv = document.getElementById('stopwatch'),
+		countdownDiv = document.getElementById('countdown');
+
+	var cdt, h2 = document.getElementById('countdownTimer'),
+		startcd = document.getElementById('startCD'),
+		pausecd = document.getElementById('pauseCD'),
+		stopcd = document.getElementById('stopCD');
 
 
-	$("#stopwatchBtn").click(function(e){
-		$("#countdown").hide();
-		$("#stopwatch").show();
-		$("#countdownBtn").removeClass("active");
-		$(this).addClass("active");
-		$("#pauseCD").trigger("click");
-	});
-
-	$("#countdownBtn").click(function(e){
-		$("#stopwatch").hide();
-		$("#countdown").show();
-		$("#resetSW").trigger("click");
-		$("#stopwatchBtn").removeClass("active");
-		$(this).addClass("active");
-	});
-
-	$('#startSW').click(function(){
-		$('#stopwatchTimer').stopwatch().stopwatch('start');
-	});
-
-	$('#stopSW').click(function(){
-		$('#stopwatchTimer').stopwatch().stopwatch('stop');
-	});
-
-	$('#resetSW').click(function(){
-		$('#stopwatchTimer').stopwatch().stopwatch('reset', function(){
-			$('#stopwatchTimer').stopwatch().stopwatch('stop');
-			$('#stopwatchTimer').stopwatch()[0].innerHTML = "00:00:00";
-		});
-	});
-
-	$("#startCD").click(function(){
-		// $("#countdownTimer").html("");
-		var mins = parseInt($("input[name='minutes']").val());
-		var secs = parseInt($("input[name='seconds']").val());
-		if(isNaN(mins) || isNaN(secs)){
-			alert("Enter valid minutes and seconds.");
-			return;
+	function add() {
+		msecs++;
+		if (msecs >= 100) {
+			msecs = 0;
+			secs++;
+			if (secs >= 60) {
+				secs = 0;
+				minutes++;
+			}
 		}
-		var seconds = (mins * 60) + secs;
-		// alert(seconds);
-		$("#countdownTimer").backward_timer({
-			seconds: seconds,
-			on_exhausted: function(timer) {
-    			// timer.target.text('I am exhausted. Reset me!')
-    			countdownTimeUp();
-  			}
-		});
-		$("#countdownTimer").backward_timer('start');
-	});
 
-	$("#pauseCD").click(function(){
-		// $("#countdownTimer").html("");
-		$("#countdownTimer").backward_timer('cancel');
-	});
+		h1.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (secs ? (secs > 9 ? secs : "0" + secs) : "00") + ":" + (msecs > 9 ? msecs : "0" + msecs);
 
-	$("#stopCD").click(function(){
-		// $("#countdownTimer").html("");
-		$("#countdownTimer").backward_timer('reset');
-	});
+		timer();
+	}
+	function timer() {
+		t = setTimeout(add, 10);
+	}
+	/* Start button */
+	start.onclick = timer;
 
-	function countdownTimeUp(){
-		alert("Time Up!");
+	/* Stop button */
+	stop.onclick = function() {
+		clearTimeout(t);
 	}
 
-	$("input[name='minutes']").change(function(){
-		var val = $(this).val();
-		if(isNaN(val)){
-			alert("Please enter valid number.");
-		}
-		if(val > 10){
-			alert("Max allowed minutes are 10");
-			$(this).val("");
-		}
-	});
+	/* Clear button */
+	clear.onclick = function() {
+		clearTimeout(t);
+		h1.textContent = "00:00:00";
+		msecs = 0; minutes = 0; secs = 0;
+	}
 
-	$("input[name='seconds']").change(function(){
-		var val = $(this).val();
-		if(isNaN(val)){
-			alert("Please enter valid number.");
+	switch_sw.onclick = function(){ // switch to stopwatch 
+		countdownDiv.style.display = "none";
+		stopwatchDiv.style.display = "block";
+		switch_cd.classList.remove("active");
+		switch_sw.classList.add("active");
+		msecs = 0; minutes = 0; secs = 0;
+		clearTimeout(cdt);
+		clearTimeout(t);
+	}
+
+	switch_cd.onclick = function(){ // switch to countdown timer
+		countdownDiv.style.display = "block";
+		stopwatchDiv.style.display = "none";
+		switch_sw.classList.remove("active");
+		switch_cd.classList.add("active");
+		msecs = 0; minutes = 0; secs = 0;
+		clearTimeout(cdt);
+		clearTimeout(t);
+	}
+
+	var checkCountDownTime = function(time){ // validate countdown timer with regex
+		var re = /^(0[0-9]|10):(59|[0-5][0-9]):([0-9][0-9])?$/g;
+		// regex allows max 10 mins, max 59 secs, and max 99 msecs 
+		return re.test(time);
+	}
+
+	// Countdown Timer
+
+	function subtract(){
+		var callAgain = true;
+		msecs--;
+		if (msecs <= 0) {
+			msecs = 100;// set to 100 per sec as the timeout in the cdtimer() has been set to 10msecs.
+			secs--;
+			if (secs <= 0) {
+				secs = 60;
+				minutes--;
+				if(minutes <= 0){
+					clearTimeout(cdt);
+					setCDTime();
+					callAgain = false;
+				}
+			}
 		}
-		if(val > 59){
-			alert("Max allowed seconds are 59");
-			$(this).val("");
+
+		h2.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (secs ? (secs > 9 ? secs : "0" + secs) : "00") + ":" + (msecs > 9 ? msecs : "0" + msecs);
+		if(callAgain){
+			cdtimer();
 		}
-	});
-});
+		else{
+			alert("Time up!!");
+		}
+	}
+
+	function cdtimer(){
+		cdt = setTimeout(subtract, 10);// updates timer every 10 msecs
+	}
+
+	function setCDTime(){ // get input from text box and parse it as mins, secs, msecs
+		var timeInp = document.getElementById('cdTime');
+		if(checkCountDownTime(timeInp.value)){
+			var digs = timeInp.value.trim().toString().split(":");
+			msecs = parseInt(digs[2]);
+			secs = parseInt(digs[1]);
+			minutes = parseInt(digs[0]);
+			return true;
+		}
+		else{
+			alert("Invalid time. Max 10 minutes countdown allowed. Max mins: 10, Max secs: 59, Max msecs: 99 ");
+			return false;
+		}
+	}
+
+	startcd.onclick = function(){ // start countdown click handler
+		if(setCDTime()){
+			cdtimer();
+		}
+	}
+
+	pausecd.onclick = function(){ // pause countdown click handler
+		clearTimeout(cdt);
+	}
+
+	stopcd.onclick = function(){ // stop/reset countdown click handler
+		clearTimeout(cdt);
+		h2.textContent = "00:00:00";
+		msecs = 0; minutes = 0; secs = 0;
+		setCDTime();
+	}
+
+})();
